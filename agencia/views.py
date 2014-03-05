3557b7b2094d4d4c3b5621c9d015c1195750382a
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-03-05 10:07:56 -0800

    Se adapta vista y template de forma que se respete si se utiliza el sitio estándar o un sitio externo

--- agencia/views.py
+++ agencia/views.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from iampacks.agencia.trabajo.models import Trabajo, ItemPortfolio
 from django.utils.translation import ugettext as _
 from django.utils.translation import ugettext_lazy
 from iampacks.cross.usuario.signals import usuario_after_register_before_redirect 
+from django.conf import settings
 
 def notify_register(sender,request,**kwargs):
   messages.info(request,_(u'Por favor atualize os dados do seu perfil a ser analizado por nossa agencia.'))
@@ -13,6 +14,8 @@ def notify_register(sender,request,**kwargs):
 usuario_after_register_before_redirect.connect(notify_register)
 
 def index(request):
+  if settings.AMBIENTE.sitio.externo.url:
+    return redirect(settings.AMBIENTE.sitio.externo.url)
   trabajos = Trabajo.objects.filter(publicado=True).order_by('-fecha_ingreso')[:3]
   portfolio = ItemPortfolio.objects.order_by('-fecha')[:3]
   return render(request,'agencia/index.html', { 'trabajos': trabajos, 'portfolio': portfolio})
