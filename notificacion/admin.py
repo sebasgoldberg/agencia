be77cbdbc79e4902230bcbf9f54adb231698eced
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-05-17 07:02:48 -0700

    Se agregan las notificaciones al backend administrativo de la aplicación.

--- notificacion/admin.py
+++ notificacion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from iampacks.agencia.notificacion.models import NotificacionCuentaAgenciadoExistente
 
-# Register your models here.
+
+class NotificacionCuentaAgenciadoExistenteAdmin(admin.ModelAdmin):
+  list_display=['id', 'agenciado', 'email_destinatario', 'fecha_envio']
+  list_display_links = ('id', )
+  date_hierarchy='fecha_envio'
+  search_fields=['email_destinatario']
+
+admin.site.register(NotificacionCuentaAgenciadoExistente, NotificacionCuentaAgenciadoExistenteAdmin)
